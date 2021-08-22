8b82304acc50cd15d9d81a0b427a7d49b4ef6393
alayek <5607371+alayek@users.noreply.github.com>
2021-08-22 23:20:08 +0530

    add navbar

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  darkMode: "media", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
