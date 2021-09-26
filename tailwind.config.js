b8d80870fbdcf3b9ef7b9d7248deb08e8c3b3c80
alayek <5607371+alayek@users.noreply.github.com>
2021-09-26 11:19:21 +0530

    feat: finalize tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors');
+// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
-  purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
-  darkMode: "media", // or 'media' or 'class'
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      nunito: ['Nunito', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+        teal: colors.teal,
+        sky: colors.sky,
+        fuschia: colors.fuchsia,
+        white: colors.white,
+        orange: colors.orange,
+        amber: colors.amber,
+        nitrate: {
+          DEFAULT: '#5D5FEF',
+          text: '#20272E',
+          dark: '#2C2D30',
+          'really-dark': '#20272E',
+          'soft-dark': '#282F36',
+          'softer-dark': '#8C8989',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
